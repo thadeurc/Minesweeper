9785cd16f2f51a2b18d6b42a49292398b3d4e4d0
dojo-lab-br <thadeurc@gmail.com>
2011-07-28 23:23:56 -0300

    Incomplete version. It is in the state we left at end of the session.

--- main/java/src/br/lab/dojo/minesweeper/Minesweeper.java
+++ main/java/src/br/lab/dojo/minesweeper/Minesweeper.java
@@ -1,13 +1,47 @@
 package br.lab.dojo.minesweeper;
 
+import java.util.Arrays;
 
 public class Minesweeper {
 
 	public static final char BOMB = '*';
-	public static final char CLEAR = '.';	
+	public static final char CLEAR = '.';
 	private char[][] matrix;
 
+	public void validateArray(char[] array) {
+		for (int i = 0; i < array.length; i++) {			
+			if (array[i] != BOMB && array[i] != CLEAR) {
+				throw new IllegalArgumentException();
+			}
+		}
+	}
+
 	public Minesweeper(char[][] matrix) {
-		this.matrix = matrix; 
+		char[][] newMatrix = new char[matrix.length][];
+		for (int i = 0; i < matrix.length; i++) {
+			char[] c = matrix[i];
+			validateArray(c);
+			newMatrix[i] = Arrays.copyOf(c, c.length);
+			
+		}
+		this.matrix = newMatrix;
+	}
+
+	public boolean isBomb(int coll, int row) {
+		return this.matrix[coll][row] == BOMB;
+	}
+
+	public char[][] process() {
+		return this.matrix;
+	}
+
+	public boolean isValidPosition(int row, int col) {
+		if (col < 0 && row < 0) {
+			return false;
+		} 
+		if (col >= matrix.length && row >= matrix[0].length) {
+			return false;
+		}
+		return true;
 	}
 }
--- test/java/src/br/lab/dojo/minesweeper/MinesweeperTest.java
+++ test/java/src/br/lab/dojo/minesweeper/MinesweeperTest.java
@@ -1,6 +1,57 @@
 package br.lab.dojo.minesweeper;
 
+import junit.framework.Assert;
+
+import org.junit.Test;
+
 
 public class MinesweeperTest {
+
+	@Test
+	public void shouldNotExposeState(){
+		char [][] matrix = new char[][]{{Minesweeper.BOMB}};
+		Minesweeper mine = new Minesweeper(matrix);
+		matrix[0][0] = Minesweeper.CLEAR;
+		char [][] result = mine.process();
+		Assert.assertEquals(result[0][0], Minesweeper.BOMB);
+		Assert.assertNotSame(matrix, result);
+	}
 	
+	@Test(expected=IllegalArgumentException.class)
+	public void shouldRejectNotValidChar(){
+		char [][] matrix = new char[][]{{','}};
+		new Minesweeper(matrix);				
+	}
+	
+	@Test
+	public void shouldAcceptValidChar(){
+		char [][] matrix = new char[][]{{Minesweeper.BOMB}, {Minesweeper.CLEAR}};
+		new Minesweeper(matrix);
+		Assert.assertTrue(true);
+	}
+	
+	@Test
+	public void testIsBomb(){
+		char [][] matrix = new char[][]{{Minesweeper.BOMB}, {Minesweeper.CLEAR}};
+		Minesweeper mine = new Minesweeper(matrix);
+		Assert.assertTrue(mine.isBomb(0,0));
+	}
+	
+	@Test
+	public void testIsNotBomb(){
+		char [][] matrix = new char[][]{{Minesweeper.BOMB}, {Minesweeper.CLEAR}};
+		Minesweeper mine = new Minesweeper(matrix);
+		Assert.assertFalse(mine.isBomb(1,0));
+	}
+	
+	@Test
+	public void isInvalidPosition() {
+		char [][] matrix = new char[][]{
+				{Minesweeper.BOMB, Minesweeper.CLEAR},
+				{Minesweeper.BOMB, Minesweeper.CLEAR}
+				};
+		Minesweeper mine = new Minesweeper(matrix);
+		
+		Assert.assertTrue(mine.isValidPosition(0,1));
+	}
 }
